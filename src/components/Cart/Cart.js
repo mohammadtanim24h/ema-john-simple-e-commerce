aa92a00466fb0d80ff3e301f7d2576b221a7c6ca
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-23 19:53:26 +0600

    fixed quantity while adding product

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,8 +4,10 @@ import './Cart.css';
 const Cart = ({ cart }) => {
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart) {
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + (product.price * product.quantity);
         shipping = shipping + product.shipping;
     }
     const tax = parseFloat((total * .10).toFixed(2));
@@ -13,7 +15,7 @@ const Cart = ({ cart }) => {
     return (
         <div className="cart">
             <h3 className="cart-title">Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: {shipping}</p>
             <p>Tax: {tax}</p>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,10 +12,38 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
-    const handleAddToCart = product => {
-        const newCart = [...cart, product];
+
+    // get local storage cart
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
+    // handle addtocart
+    const handleAddToCart = selectedProduct => {
+        debugger;
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if(!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id); // jei product ta already ase oita chara bakigula ke nise tarpor jeita ase oitar quantity baraise. tarpor baki product er array ta ke spread kore ager product ta jug kore dise. eita kora hoise jate product ta copy na hoye jay.
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className='shop-container'>
